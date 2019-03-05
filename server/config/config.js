1fbe25f5405bb8f95ea5e03a2b6be1a14ac25a46
Agu <agustinnassiff@gmail.com>
2019-03-05 17:53:30 -0300

    Variable de entorno Mongo

--- server/config/config.js
+++ server/config/config.js
@@ -14,6 +14,6 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 let urlDB = '';
 
 if (process.env.NODE_ENV == 'dev') urlDB = 'mongodb://localhost:27017/cafe';
-else urlDB = 'mongodb+srv://strider:wUHQv9h9bsf2L25J@cluster0-sjc6r.mongodb.net/cafe';
+else urlDB = process.env.MongoDB_URL;
 
 process.env.URLDB = urlDB;
